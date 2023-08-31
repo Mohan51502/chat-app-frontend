8b163ab9957b93d9aded36d0822c7b0041db68b3
Mohan51502 <rajmohan51502@gmail.com>
2023-08-31 13:11:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,82 +1,18 @@
-import Header from "./components/Header";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Dashboard from "./components/Dashboard";
-import Error from "./components/Error";
-import PasswordReset from "./components/PasswordReset";
-import ForgotPassword from "./components/ForgotPassword";
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
-import { Routes, Route, useNavigate } from "react-router-dom"
-import { useEffect, useContext, useState } from "react";
-import { LoginContext } from "./components/ContextProvider/Context";
-
-
-function App() {
-
-  const [data, setData] = useState(false);
-
-  const { logindata, setLoginData } = useContext(LoginContext);
-
-
-  const history = useNavigate();
-
-  const DashboardValid = async () => {
-    let token = localStorage.getItem("usersdatatoken");
-
-    const res = await fetch("https://day43backend.onrender.com/validuser", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": token
-      }
-    });
-
-    const data = await res.json();
-
-    if (data.status == 401 || !data) {
-      console.log("user not valid");
-    } else {
-      console.log("user verify");
-      setLoginData(data)
-      history("/dash");
-    }
-  }
-
-  useEffect(() => {
-    setTimeout(()=>{
-      DashboardValid();
-      setData(true)
-    },2000)
-
-  }, [])
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SetAvatar from "./components/SetAvatar";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+export default function App() {
   return (
-    <>
-      {
-        data ? (
-          <>
-            <Header />
-
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/dash" element={<Dashboard />} />
-              <Route path="/password-reset" element={<PasswordReset />} />
-              <Route path="/forgotpassword/:id/:token" element={<ForgotPassword />} />
-              <Route path="*" element={<Error />} />
-            </Routes>
-          </>
-
-        ) : <Box sx={{ display: 'flex', justifyContent: "center", alignItems: "center", height: "100vh" }}>
-          Loading... &nbsp;
-          <CircularProgress />
-        </Box>
-      }
-
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/setAvatar" element={<SetAvatar />} />
+        <Route path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
